7025687ff5d57fb02a69143923b5b536cacc6d7c
AlessandroParente1 <alessandroparente02@gmail.com>
2024-09-03 19:26:48 +0200

    Revert "Carica progetto"
    This reverts commit 1e2260d6653fb004aa8bd196069d8acd9fabd680.

--- client/src/pages/index.jsx
+++
@@ -1,8 +0,0 @@
-import ViewAllProjects from "./projects";
-
-const Root = () => {
-  // Root component that renders the ViewAllProjects component
-  return <ViewAllProjects />;
-};
-
-export default Root;
--- server/routes/seed.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { seedDatabase } from "../controllers/seed.controller.js";
-
-const router = express.Router();
-
-router.post("/", seedDatabase);
-
-export default router;
