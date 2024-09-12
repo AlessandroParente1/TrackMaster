2d6242131936ab1f9932bcb2e2270e1516e42504
AlessandroParente1 <alessandroparente02@gmail.com>
2024-09-12 21:25:34 +0200

    web service

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    // Uncomment the following line to build a static site.
+    // output: "export",
 
     reactStrictMode: true,
 };
