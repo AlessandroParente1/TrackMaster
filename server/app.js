56a067c88ca0e7e4b0c4a0a756e565810cfcb890
AlessandroParente1 <alessandroparente02@gmail.com>
2024-09-19 17:30:30 +0200

    socket.io started

--- server/app.js
+++ server/app.js
@@ -1,3 +1,7 @@
+//Socket.io
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
@@ -10,6 +14,28 @@ import commentRoutes from "./routes/comment.route.js";
 import { handleError, routeNotFound, authMiddleware } from './middleware/middleware.js';
 
 const app = express();
+
+const server = createServer(app);  // Create a HTTP server
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Socket.IO connection
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+
+    // Example of listening to and emitting messages
+    socket.on('message', (message) => {
+        io.emit('message', message);
+    });
+});
 
 //preconfigure express app
 app.use(express.json());
